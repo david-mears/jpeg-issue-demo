0acda91b1f51f9856719f4a7ee56a59ac8aed5ce
David Mears <david.mears@digital.education.gov.uk>
2020-08-16 03:05:14 +0100

    Fix typo

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  has_many :cups, dependent: destroy
-  has_many :players, dependent: destroy
+  has_many :cups, dependent: :destroy
+  has_many :players, dependent: :destroy
   validates :slug, presence: true
 end
