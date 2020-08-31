db73de7d24d1764a4eff96525e09e2080ba66ca5
David Mears <david.mears@digital.education.gov.uk>
2020-08-31 22:23:39 +0100

    Route root to games index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'games#index'
+  
   resources :games, only: %i[index create new show], param: :slug
   post '/games/find', to: 'games#find'
 end
