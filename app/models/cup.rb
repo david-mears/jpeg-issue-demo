6511304604619db4e7cd1150aa3707c0390e3481
David Mears <david.mears@digital.education.gov.uk>
2020-09-01 23:26:29 +0100

    Read cup filenames from folder

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -9,17 +9,11 @@ class Cup < ApplicationRecord
     holy_grail: 'The Holy Grail'
   }.freeze
 
-  # rubocop:disable Layout/LineLength
-  FILENAMES = ['another cup.jpg', 'chalice6.jpg', 'jewel cup.jpg', 'big_cup.jpg', 'chalice7.jpg', 'kiddush 2.jpg', 'cedarware cup.jpg', 'cup 8.jpeg', 'kiddush 3.jpg', 'chalice times.gif', 'cup2.jpg', 'kiddush 4.jpg', 'chalice-web.jpg', 'funny cup.jpg', 'kiddush.jpg', 'chalice.jpg', 'glasscup.jpg', 'metal cup.jpg', 'chalice2.jpg', 'goldcup.jpeg', 'metalcup.jpeg', 'chalice3.jpg', 'golden chalice no whitebackground.png', 'mug.png', 'chalice4.jpg', 'ivory cup.jpg', 'stonecup.jpg', 'chalice5.jpeg', 'ivory cups.jpeg'].freeze
-  # rubocop:enable Layout/LineLength
-
   enum kind: { accursed_chalice: 0, merlins_goblet: 1, holy_grail: 2 }
 
   def self.read_n_random_filenames(number_of_filenames)
-    filenames = FILENAMES.dup
-    scratch = []
-    number_of_filenames.times { scratch.push filenames.shuffle!.pop }
-    scratch
+    filenames = Dir.entries("./app/assets/images/cups").reject { |file| File.directory? file }
+    filenames.shuffle.take(number_of_filenames)
   end
 
   def human_readable_name
